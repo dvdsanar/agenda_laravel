39cbe258ca7f01d69eb45bc7c063df926518e5b9
David S <dvdsanar@gmail.com>
2022-05-04 10:56:56 +0200

    feat: contact seeder added

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB ::table('contacts')->insert(
+            [
+                'name' => 'Antonio',
+                'surname' => 'Garcia',
+                'email'=> 'antonio@antonio.com',
+                'phone_number'=>'123456789',
+                'id_user'=> 1
+            ]
+            );
+    }
+}
