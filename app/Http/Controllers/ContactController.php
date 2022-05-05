f6389b85ed5e898294738bce55c6223bda9bc511
David S <dvdsanar@gmail.com>
2022-05-05 10:15:21 +0200

    feat: try/catch en todos las funciones del contactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -35,93 +35,109 @@ class ContactController extends Controller
 
     public function getContactById ($id)
     {
-        // $contact = DB::table('contacts')->where('id_user','=', 1)->where('id','=', $id)->get()->toArray();
-        //$contact = DB::table('contacts')->where('id_user','=', 7)->find($id);
+        try {
+            // $contact = DB::table('contacts')->where('id_user','=', 1)->where('id','=', $id)->get()->toArray();
+            //$contact = DB::table('contacts')->where('id_user','=', 7)->find($id);
 
-        $contact = Contact::where('id_user', 1)->where('id', $id)->first();
+            $contact = Contact::where('id_user', 1)->where('id', $id)->first();
 
-        if (empty($contact)) {
-            return response()->json([
-                "error" => "Este contacto no existe"
-            ],
-            404);
+            if (empty($contact)) {
+                return response()->json([
+                    "error" => "Este contacto no existe"
+                ],
+                404);
+            }
+
+            return response()->json($contact, 200);
+        } catch (\Throwable $th) {
+            return response()->json(["error" => "ups"], 500);
         }
-
-        return response()->json($contact, 200);
     }
 
     public function postContact (Request $request)
     {
-        //dump($request->all()); //pasar informacion por body y ver por console log todo
-        //dump($request->all()['name']); //pasar informacion por body y ver por console log una clave de la request
-        
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string',
-            'surname' => 'required|string',
-            'phone_number' => 'required|string',
-            'email' => 'required|email'
-        ]);
- 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
+        try {
+            //dump($request->all()); //pasar informacion por body y ver por console log todo
+            //dump($request->all()['name']); //pasar informacion por body y ver por console log una clave de la request
+            
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string',
+                'surname' => 'required|string',
+                'phone_number' => 'required|string',
+                'email' => 'required|email'
+            ]);
+    
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+
+
+            $newContact = new Contact();
+
+            $newContact->name = $request->name;
+            $newContact->surname = $request->surname;
+            $newContact->phone_number = $request->phone_number;
+            $newContact->email = $request->email;
+            $newContact->id_user = $request->id_user;
+
+            $newContact->save();
+
+            
+            return response()->json(["data"=>$newContact, "success"=>"Contacto Creado"], 200);
+        } catch (\Throwable $th) {
+            return response()->json(["error" => "ups"], 500);
         }
-
-
-        $newContact = new Contact();
-
-        $newContact->name = $request->name;
-        $newContact->surname = $request->surname;
-        $newContact->phone_number = $request->phone_number;
-        $newContact->email = $request->email;
-        $newContact->id_user = $request->id_user;
-
-        $newContact->save();
-
-        
-        return response()->json(["data"=>$newContact, "success"=>"Contacto Creado"], 200);
     }
 
     public function putContact (Request $request, $id)
     {
-        $contact = Contact::where('id', $id)->where('id_user', 1)->first();
+        try {
+            $contact = Contact::where('id', $id)->where('id_user', 1)->first();
 
-        if (empty($contact)) {
-            return response()->json([
-                "error" => "Este contacto no existe"
-            ],
-            404);
+            if (empty($contact)) {
+                return response()->json([
+                    "error" => "Este contacto no existe"
+                ],
+                404);
+            }
+            
+            if (isset($request->name))
+                $contact->name = $request->name;
+
+            if (isset($request->surname))
+                $contact->surname = $request->surname;
+
+            if (isset($request->phone_number))
+                $contact->phone_number = $request->phone_number;
+
+            if (isset($request->email))
+                $contact->email = $request->email;
+
+            $contact->save();
+
+            return response()->json(["data"=>$contact, "success"=>"Contacto Actualizado"], 200);
+        } catch (\Throwable $th) {
+            return response()->json(["error" => "ups"], 500);
         }
-        
-        if (isset($request->name))
-            $contact->name = $request->name;
-
-        if (isset($request->surname))
-            $contact->surname = $request->surname;
-
-        if (isset($request->phone_number))
-            $contact->phone_number = $request->phone_number;
-
-        if (isset($request->email))
-            $contact->email = $request->email;
-
-        $contact->save();
-
-        return response()->json(["data"=>$contact, "success"=>"Contacto Actualizado"], 200);
     }
 
     public function deleteContact ($id)
     {
-        $contact = Contact::where('id', $id)->where('id_user', 1)->first();
+        try {
+            $contact = Contact::where('id', $id)->where('id_user', 1)->first();
 
-        if (empty($contact)) {
-            return response()->json([
-                "error" => "Este contacto no existe"
-            ],
-            404);
+            if (empty($contact)) {
+                return response()->json([
+                    "error" => "Este contacto no existe"
+                ],
+                404);
+            }
+
+            $contact->delete();
+
+            return response()->json([ "success"=>"Has eliminado el contacto: ".$id], 200);
+        } catch (\Throwable $th) {
+            return response()->json(["error" => "ups"], 500);
         }
-
-        $contact->delete();
-
-        return response()->json([ "success"=>"Has eliminado el contacto: ".$id], 200);
     }
 }
